4688ae5489b0832f9798db3079273fdaceda2b3f
t-ohtsuka89 <102138393+t-ohtsuka89@users.noreply.github.com>
2022-08-22 16:08:51 +0900

    fix: sqlの修正

--- flyway/sql/V1.0.0__init.sql
+++ flyway/sql/V1.0.0__init.sql
@@ -1,10 +1,10 @@
 -- テーブル作成
 -- ユーザーテーブル
 CREATE TABLE `users` (
-                         `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+                         `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
                          `username` VARCHAR(256) NOT NULL COMMENT 'ユーザーID',
                          `email` VARCHAR(256) NOT NULL COMMENT 'メールアドレス',
-                         `password` VARCHAR(256) NOT NULL COMMENT 'パスワード',
+                         `password` BINARY(60) NOT NULL COMMENT 'パスワード (BCrypt)',
                          `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '登録日時',
                          `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
                          `deleted_at` datetime DEFAULT NULL COMMENT '削除日時',
@@ -12,8 +12,8 @@ CREATE TABLE `users` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci comment = 'ユーザーテーブル';
 -- 投稿テーブル
 CREATE TABLE `posts` (
-                         `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
-                         `user_id` int unsigned NOT NULL COMMENT '投稿者ユーザーID',
+                         `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
+                         `user_id` int NOT NULL COMMENT '投稿者ユーザーID',
                          `text` VARCHAR(140) NOT NULL COMMENT '投稿本文',
                          `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '登録日時',
                          `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
@@ -23,16 +23,16 @@ CREATE TABLE `posts` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci comment = '投稿テーブル';
 -- いいねテーブル
 CREATE TABLE `favorites` (
-                             `user_id` int unsigned NOT NULL COMMENT 'ユーザーID',
-                             `post_id` int unsigned NOT NULL COMMENT '投稿ID',
+                             `user_id` int NOT NULL COMMENT 'ユーザーID',
+                             `post_id` int NOT NULL COMMENT '投稿ID',
                              FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
                              FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
                              PRIMARY KEY (`user_id`, `post_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci comment = 'いいねテーブル';
 
 -- データ挿入
-INSERT INTO users (username, email, password) VALUES ('山田太郎', 'test@example.com', 'pass123');
-INSERT INTO users (username, email, password) VALUES ('田中花子', 'test2@example.com', 'pass123');
+INSERT INTO users (username, email, password) VALUES ('山田太郎', 'test@example.com', '$2a$08$W0kLGEd0VolF.ZUXlub9ge6iI0QQVbmgtiuCw0ijFJjfo0ZkJmLta');
+INSERT INTO users (username, email, password) VALUES ('田中花子', 'test2@example.com', '$2a$08$W0kLGEd0VolF.ZUXlub9ge6iI0QQVbmgtiuCw0ijFJjfo0ZkJmLta');
 INSERT INTO posts (user_id, text) VALUES (1, '今日の晩御飯は美味しかった。by 山田太郎');
 INSERT INTO posts (user_id, text) VALUES (2, '今日の晩御飯はまずかった。 by 田中花子');
 # 太郎から花子の投稿へのいいね
