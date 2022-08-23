8ce14ba88153e6e78826930424688e43db6e80b5
t-ohtsuka89 <102138393+t-ohtsuka89@users.noreply.github.com>
2022-08-23 18:29:13 +0900

    style:単一名のインポートを使用

--- src/main/kotlin/com/example/demo/app/controller/PostController.kt
+++ src/main/kotlin/com/example/demo/app/controller/PostController.kt
@@ -4,7 +4,11 @@ package com.example.demo.app.controller
 import com.example.demo.domain.entity.Post
 import com.example.demo.domain.repository.PostRepository
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
 
