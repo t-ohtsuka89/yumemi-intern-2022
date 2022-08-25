4aa3f341bbaa30b82dc3f10b63605647798aaffc
t-ohtsuka89 <102138393+t-ohtsuka89@users.noreply.github.com>
2022-08-25 10:04:47 +0900

    fix:パッケージ名の修正

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.example.test-project
+package com.example.demo
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
